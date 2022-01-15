fff8bfccd398dfcc4f0f7dfe7a45a6c66e2e7319
Ali Irani <aliirani74@gmail.com>
2022-01-15 19:20:14 +0330

    Add user and account models

---
+++ accountManagementService/bankAccounts/models/account.py
@@ -0,0 +1,13 @@
+from django.db import models
+from base import BaseModel
+from user import User
+
+
+class Account(BaseModel):
+    class Currency(models.TextChoices):
+        USD = '1', 'USD'
+        EUR = '2', 'EUR'
+
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=5, choices=Currency.choices, default=Currency.USD)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='accounts')
---
+++ accountManagementService/bankAccounts/models/user.py
@@ -0,0 +1,8 @@
+from django.db import models
+from base import BaseModel
+
+
+class User(BaseModel):
+    name = models.CharField(max_length=100, null=True)
+    passport_id = models.CharField(max_length=15, unique=True, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=True, blank=True)
