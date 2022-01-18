df7cfe6f28ad9f5b042286d2a2b7496ab5a03fcc
Ali Irani <aliirani74@gmail.com>
2022-01-18 16:13:25 +0330

    Add name for urls

--- accountManagementService/bankAccounts/urls.py
+++ accountManagementService/bankAccounts/urls.py
@@ -3,7 +3,7 @@ from bankAccounts.views.account import AccountDetail, AccountList
 from bankAccounts.views.transaction import Transaction
 
 urlpatterns = [
-    path('', AccountList.as_view()),
-    path('<int:pk>', AccountDetail.as_view()),
-    path('transaction', Transaction.as_view())
+    path('', AccountList.as_view(), name='account-list'),
+    path('<int:pk>', AccountDetail.as_view(), name='account-details'),
+    path('transaction', Transaction.as_view(), name='transaction')
 ]
