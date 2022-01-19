2c93ce539507f525a74ad2298c6ea9290c2c698c
Ali Irani <aliirani74@gmail.com>
2022-01-19 09:12:20 +0330

    Collectstatic and add swagger

--- accountManagementService/accountManagementService/settings.py
+++ accountManagementService/accountManagementService/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
+
     'bankAccounts',
+    'currencyExchange',
 ]
 
 MIDDLEWARE = [
@@ -123,9 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(os.path.dirname(BASE_DIR), "static"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+REST_FRAMEWORK = {
+  'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
